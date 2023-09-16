0a6b6090374ade45a3e23a0ead204ff1ff0a876b
ulrich <ulrichfedjo@gmail.com>
2023-09-16 02:49:43 +0100

    ajout de slider

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -3,16 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Slider;
+
 
 class SliderController extends Controller
 {
-    public function ajouterslider(){
+    public function ajouterslider()
+    {
         return view('admin.ajouterslider');
     }
-    public function sauverslider(Request $request){
+    public function sauverslider(Request $request)
+    {
+        $this->validate($request, [
+            'description1' => 'required',
+            'description2' => 'required',
+            'slider_image' => 'image|nullable|max:1999'
+        ]);
 
-   }
-   public function sliders(){
-    return view('admin.sliders');
-   }
+        if ($request->hasFile('slider_image')) {
+            $fileNameWithExt = $request->file('slider_image')->getClientOriginalName();
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            $extension = $request->file('slider_image')->getClientOriginalExtension();
+            $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+            $path = $request->file('slider_image')->storeAs('public/slider_images', $fileNameToStore);
+        } else {
+            $fileNameToStore = 'noimage.jpg';
+        }
+
+
+        $slider = new Slider();
+        $slider->description1 = $request->input('description1');
+        $slider->description2 = $request->input('description2');
+        $slider->slider_image = $fileNameToStore;
+        $slider->statut = 1;
+
+        $slider->save();
+
+        return redirect('/ajouterslider')->with('statut', 'Le slider a été inséré avec succès!');
+    }
+    public function sliders()
+    {
+        return view('admin.sliders');
+    }
 }
--- resources/views/admin/ajouterslider.blade.php
+++ resources/views/admin/ajouterslider.blade.php
@@ -8,11 +8,26 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Ajouter un slider</h4>
+                    @if (Session::has('statut'))
+                <div class="alert alert-success">
+                    {{ Session::get('statut') }}
+                </div>
+                @endif
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
                     {!! Form::open([
                         'action' => 'App\Http\Controllers\SliderController@sauverslider',
                         'method' => 'POST',
                         'class' => 'cmxform',
                         'id' => 'commentForm',
+                        'enctype' => 'multipart/form-data'
                     ]) !!}
                     {{ csrf_field() }}
                     <div class="form-group">
